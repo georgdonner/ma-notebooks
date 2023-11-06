0b4860a51c8e9d1a3a9be71f6d36d361a9c837ae
Georg Donner <georgdonner@posteo.de>
2023-11-06 22:08:42 +0100

    Basic pyrope integration

--- fields.py
+++ fields.py
@@ -1,45 +1,44 @@
 from sympy import parse_expr
 
 all_fields = [
-  'function', 'function_example', 'depth', 'leaves', 'polynomial', 'rational', 'domain',
-  'singularities', 'singularities_count', 'limit_inf', 'limit_ninf', 'asymptotes', 'asymptotes_count', 
-  'periodicity', 'y_intercept', 'zeros', 'zeros_count', 'zeros_exact',
-  'derivative', 'integral', 'integral_elementary', 'integral_rules'
+    'function', 'function_example', 'depth', 'leaves', 'polynomial', 'rational', 'domain',
+    'singularities', 'singularities_count', 'limit_inf', 'limit_ninf', 'asymptotes', 'asymptotes_count', 
+    'periodicity', 'y_intercept', 'zeros', 'zeros_count', 'zeros_exact',
+    'derivative', 'integral', 'integral_elementary', 'integral_rules'
 ]
 
 def parse_sympy(x):
-  print(x, type(x))
-  if x == '':
-    return float('nan')
-  try:
-    return parse_expr(x)
-  except ValueError:
-    return float('nan')
+    if x == '':
+        return float('nan')
+    try:
+        return parse_expr(x)
+    except ValueError:
+        return float('nan')
 
 def parse_number(x):
-  try:
-    return float(x)
-  except ValueError:
-    return parse_sympy(x)
+    try:
+        return float(x)
+    except ValueError:
+        return parse_sympy(x)
   
 def handle_list(list_str, fn):
-  if not list_str:
-    return []
-  return [fn(x) for x in list_str.split(',')]
+    if not list_str:
+        return []
+    return [fn(x) for x in list_str.split(',')]
 
 converters = {
-  'y_intercept': parse_number,
-  'zeros': lambda x: handle_list(x, parse_number),
-  'integral_rules': lambda x: handle_list(x, str),
+    'y_intercept': parse_number,
+    'zeros': lambda x: handle_list(x, parse_number),
+    'integral_rules': lambda x: handle_list(x, str),
 }
 
 extended_converters = {
-  **converters,
-  'domain': parse_sympy,
-  'zeros_exact': parse_sympy,
+    **converters,
+    'domain': parse_sympy,
+    'zeros_exact': parse_sympy,
 }
 
-def csv_converters(mode='basic'):
-  if mode == 'extended':
-    return extended_converters
-  return converters
+def csv_converters(mode='default'):
+    if mode == 'extended':
+        return extended_converters
+    return converters
