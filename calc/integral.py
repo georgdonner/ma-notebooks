6d2dcecfa904a204fc5b1e761c735f86e86e9076
Georg <georgdonner@posteo.de>
2023-11-27 15:31:38 +0100

    Use only one alternative for integral rules

--- calc/integral.py
+++ calc/integral.py
@@ -1,20 +1,38 @@
 from sympy import integrate, Integral
 from sympy.integrals.risch import NonElementaryIntegral
 from sympy.integrals.manualintegrate import integral_steps
-from re import finditer
+
+valid_attrs = ['substep', 'substeps', 'v_step', 'second_step', 'alternatives']
+ignore_rules = ['AlternativeRule']
+
+def get_rule(step):
+    rule = type(step).__name__
+    if rule not in ignore_rules:
+        yield rule
+    for attr in valid_attrs:
+        if hasattr(step, attr):
+            if attr == 'alternatives':
+                yield from get_rule(step.alternatives[0])
+            elif attr == 'substeps':
+                for substep in step.substeps:
+                    yield from get_rule(substep)
+            else:
+                yield from get_rule(getattr(step, attr))
+
+def get_rules(steps):
+    return list(dict.fromkeys((get_rule(steps))))
 
 def integral(f, x):
     integral = None
     integral_elementary = True
-    integral_rules = set()
+    integral_rules = []
     try:
         integral = integrate(f, x, risch=True)
         if len(integral.atoms(NonElementaryIntegral)):
             integral_elementary = False
         if integral_elementary:
             steps = integral_steps(f, x)
-            for match in finditer('\w+Rule', str(steps)):
-                integral_rules.add(match.group())
+            integral_rules = get_rules(steps)
             if len(integral.atoms(Integral)) and not 'DontKnowRule' in integral_rules:
                 integral = integrate(f, x)
         if 'DontKnowRule' in integral_rules:
@@ -28,5 +46,5 @@ def integral(f, x):
     return {
         'integral': integral,
         'integral_elementary': integral_elementary,
-        'integral_rules': list(integral_rules or []),
+        'integral_rules': integral_rules,
     }
