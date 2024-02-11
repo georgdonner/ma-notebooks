c4cb99fd4d5e63d51a1f6a1d6fcf445fe4569f87
Georg Donner <georgdonner@posteo.de>
2024-02-11 16:43:58 +0100

    Support reduced source with external full source

---
+++ reduce_csv.py
@@ -0,0 +1,37 @@
+import argparse, re
+import pandas as pd
+from steckbriefe.fields import parse_discontinuity_json, csv_dtypes, handle_list
+
+parser = argparse.ArgumentParser("reduce_csv")
+parser.add_argument('input', help='input csv file with steckbriefe')
+parser.add_argument('-f', '--format', help='output file format', default='pickle', type=str, choices=['pickle', 'csv'])
+parser.add_argument('-o', '--output', help='output file for steckbriefe', type=str)
+args = parser.parse_args()
+if not args.output:
+    args.output = re.sub(r'\w+$', args.format, args.input)
+
+dtypes = csv_dtypes()
+csv_converters = {
+    'discontinuities': parse_discontinuity_json,
+    'integral_rules': lambda x: handle_list(x, str),
+}
+check_exist = ['domain', 'range', 'periodicity', 'derivative', 'integral', 'inverse']
+for field in check_exist:
+    csv_converters[field] = lambda val: bool(val)
+
+df = pd.read_csv(args.input, dtype=dtypes, converters=csv_converters)
+# Expand integral rules
+df = df.join(df['integral_rules'].str.join(',').str.get_dummies(sep=',').astype(bool))
+# Expand discontinuity types
+df['disc_types'] = df['discontinuities'].apply(lambda arr: [d['type'] for d in arr])
+df = df.join(df['disc_types'].str.join(',').str.get_dummies(sep=',').astype(bool))
+# Retain function examples
+function_col = df['function_example']
+# Only include easily indexable values like numbers and booleans
+reduced = df.select_dtypes(exclude=['object'])
+reduced.insert(loc=0, column='function_example', value=function_col)
+
+if args.format == 'csv':
+    reduced.to_csv(args.output)
+elif args.format == 'pickle':
+    reduced.to_pickle(args.output)
--- steckbriefe/dataset.py
+++ steckbriefe/dataset.py
@@ -1,40 +1,70 @@
+from functools import wraps
+from io import StringIO
 import pandas as pd
 from steckbriefe.fields import csv_converters, csv_dtypes
 from steckbriefe.steckbrief import Steckbrief
 
+def not_external(fn):
+    @wraps(fn)
+    def decorated(*args, **kwargs):
+        if args[0].external_source:
+            raise ValueError('method not supported with external source')
+        return fn(*args, **kwargs)
+    return decorated
+
 class Dataset:
-    def __init__(self, dataframe=None, source='steckbriefe.csv', mode='default'):
+    def __init__(self, dataframe=None, source='steckbriefe.csv', external_source=None, mode='default'):
         if dataframe is not None:
             self.df = dataframe
         else:
-            self.df = pd.read_csv(source, converters=csv_converters(mode=mode), dtype=csv_dtypes())
+            if source.endswith('csv'):
+                self.df = pd.read_csv(source, converters=csv_converters(mode=mode), dtype=csv_dtypes())
+            elif source.endswith('pickle'):
+                self.df = pd.read_pickle(source)
+            else:
+                raise ValueError('unsupported source type')
+        self.external_source = external_source
 
     #### GENERAL UTILITY FUNCTIONS ####
 
     def random_steckbrief(self):
         if self.df.empty:
             return None
-        return Steckbrief(self.df.sample(1).to_dict('records')[0])
+        sampled = self.df.sample(1)
+        if self.external_source:
+            index = sampled.index.astype(int)[0]
+            csv_string = ''
+            with open(self.external_source, 'r') as f:
+                for i, line in enumerate(f):
+                    if i == 0:
+                        csv_string += line
+                    elif i == index + 1:
+                        csv_string += line
+                        break
+            sampled = pd.read_csv(StringIO(csv_string), converters=csv_converters(mode='extended'), dtype=csv_dtypes())
+        return Steckbrief(sampled.to_dict('records')[0])
     
     def exists(self, column, value=True):
         filtered = self.df[self.df[column].notna() if value == True else self.df[column].isna()]
-        return Dataset(dataframe=filtered)
+        return Dataset(dataframe=filtered, external_source=self.external_source)
     
     def is_equal(self, column, value, negate=False):
         included = self.df[column] == value
         filtered = self.df[~included if negate else included]
-        return Dataset(dataframe=filtered)
+        return Dataset(dataframe=filtered, external_source=self.external_source)
     
     def has(self, column, value, negate=False):
+        if self.external_source:
+            return self.is_equal(value, True, negate=negate)
         exploded = self.df.explode(column)
         filtered = exploded[exploded[column] == value]
         included = self.df.index.isin(filtered.index)
         filtered = self.df[~included if negate else included]
-        return Dataset(dataframe=filtered)
+        return Dataset(dataframe=filtered, external_source=self.external_source)
     
     def apply(self, column, fn):
         filtered = self.df[self.df.apply(lambda x: fn(x[column]), axis=1)]
-        return Dataset(dataframe=filtered)
+        return Dataset(dataframe=filtered, external_source=self.external_source)
     
     def filter_numerical(self, column, query, negate=False):
         if type(query) == int or type(query) == float:
@@ -47,13 +77,15 @@ class Dataset:
     def join(self, dataset):
         joined = pd.concat([self.df, dataset.df])
         joined = joined[~joined.index.duplicated()]
-        return Dataset(dataframe=joined)
+        return Dataset(dataframe=joined, external_source=self.external_source)
     
     #### BASIC PROPERTIES ####
 
+    @not_external
     def domain_reals(self, value=True):
         return self.apply('domain', lambda val: str(val) == 'Reals' if value else str(val) != 'Reals')
 
+    @not_external
     def range_reals(self, value=True):
         return self.apply('range', lambda val: str(val) == 'Reals' if value else str(val) != 'Reals')
 
@@ -97,22 +129,29 @@ class Dataset:
     
     #### EXACT VALUES ####
 
+    @not_external
     def y_intercept(self, query, negate=False):
         return self.filter_numerical('y_intercept', query, negate=negate)
     
+    @not_external
     def limit_infinity(self, query, negate=False):
         return self.filter_numerical('limit_inf', query, negate=negate)
     
+    @not_external
     def limit_negative_infinity(self, query, negate=False):
         return self.filter_numerical('limit_ninf', query, negate=negate)
     
+    @not_external
     def has_zero(self, value, negate=False):
         return self.has('zeros', value, negate=negate)
     
+    @not_external
     def has_discontinuity(self, value, negate=False):
         return self.discontinuity_prop('value', value, negate=negate)
     
     def has_discontinuity_type(self, value, negate=False):
+        if self.external_source:
+            return self.is_equal(value, True, negate=negate)
         return self.discontinuity_prop('type', value, negate=negate)
     
     #### COUNTS ####
@@ -154,5 +193,5 @@ class Dataset:
         filtered = exploded[exploded[column] == value]
         included = self.df.index.isin(filtered.index)
         filtered = self.df[~included if negate else included]
-        return Dataset(dataframe=filtered)
+        return Dataset(dataframe=filtered, external_source=self.external_source)
     
